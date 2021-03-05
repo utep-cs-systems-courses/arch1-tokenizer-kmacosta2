f46190770dcb71be7bee2c8a6c39c1d38c867a21
kmacosta2 <kmacosta2@miners.utep.edu>
2021-03-04 17:16:45 -0700

    recent main

--- src/UImain.c
+++ src/UImain.c
@@ -8,27 +8,30 @@ int main ()
   List *listed = init_history();
   
   char currCommand[100];
-  while(currCommand[0] != 'q')
-    {
+  while(1) //while true
+  {
       printf("History test (q to Quit)>> ");
       fgets(currCommand, 100, stdin);
       if (currCommand[0] != '!')
 	{
-	  add_History(listed, currCommand);
+	  add_history(listed, currCommand);
 	}
       else if (currCommand[1] == 'h')
 	{//at index 1 expected to have the ID
 	  print_history(listed);
 	}
+      else if (currCommand[0] == 'q')
+	{
+	  free_history(listed);
+	  break;
+	}
       else/*now it has to be !# <--some number*/
 	printf("%s", get_history(listed, currCommand[1]));
-    }
-  free_history(listed);
+  } 
   //printf("%s",buffer);
   //char *strin = "My dog has fleas.";
   //char **vec = tokenize(strin);
   //print_tokens(vec);
   //free_tokens(vec);
-  
   return 0;
 }
