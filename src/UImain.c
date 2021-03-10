bf9e4544df440f4d4216514753caf68cc9766287
kmacosta2 <kmacosta2@miners.utep.edu>
2021-03-10 12:19:06 -0700

    recent version

--- src/UImain.c
+++ src/UImain.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
-
-/* "echo"ing command-line */
 int main ()
 {
   List *listed = init_history();
-  
-  char currCommand[100];
+  char curr_command[100];
   while(1) //while true
   {
       printf("History test (q to Quit)>> ");
-      fgets(currCommand, 100, stdin);
-      if (currCommand[0] != '!')
-	{
-	  add_history(listed, currCommand);
-	}
-      else if (currCommand[1] == 'h')
-	{//at index 1 expected to have the ID
-	  print_history(listed);
-	}
-      else if (currCommand[0] == 'q')
-	{
-	  free_history(listed);
-	  break;
-	}
-      else/*now it has to be !# <--some number*/
-	printf("%s", get_history(listed, currCommand[1]));
+      fgets(curr_command, 100, stdin);
+      char t = curr_command[0];
+      if (t == 'q')
+      {
+	free_history(listed);
+        break;
+      }
+      else if (curr_command[0] != '!')
+      {
+        add_history(listed, curr_command);
+      }
+       else if(curr_command[0] == '!' && curr_command[1] != 'h')
+       {
+	 int a = (int)(curr_command[1]);
+         char *s = get_history(listed, a);
+         char **vec = tokenize(s);
+	 print_tokens(vec);
+	 free_tokens(vec);
+       }
+       else if (curr_command[0] == '!' && curr_command[1] == 'h')
+       {
+	 print_history(listed);
+       }
   } 
   //printf("%s",buffer);
   //char *strin = "My dog has fleas.";
